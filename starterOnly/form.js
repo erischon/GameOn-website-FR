1d4af6720ab5beb0c5189d9955989936b4ac2b0e
Eri Schon <erischon@gmail.com>
2022-06-15 09:34:16 +0200

    form input

---
+++ starterOnly/form.js
@@ -0,0 +1,14 @@
+// Form input
+
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const locNewYork = document.getElementById("location1");
+const locSanFrancisco = document.getElementById("location2");
+const locSeattle = document.getElementById("location3");
+const locChicago = document.getElementById("location4");
+const locBoston = document.getElementById("location5");
+const locPortland = document.getElementById("location6");
+const termsOfUse = document.getElementById("checkbox1");
+const inform = document.getElementById("checkbox2");
